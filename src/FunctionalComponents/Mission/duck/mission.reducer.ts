134d0ff38b9ec843b9e506f18940b8ed7f5ed29e
hachene <jmp.juan@gmail.com>
2020-11-15 20:28:56 +0100

    Create reducers for both actions

---
+++ src/FunctionalComponents/Mission/duck/mission.reducer.ts
@@ -0,0 +1,27 @@
+import { getType } from 'typesafe-actions'
+import { addAstronaut, MissionAction, removeAstronaut } from './mission.actions'
+import { MissionState } from './mission.types'
+
+const initialState: MissionState = {
+  astronauts: [],
+}
+
+const reducer = (state: MissionState = initialState, action: MissionAction): MissionState => {
+  switch (action.type) {
+    case getType(addAstronaut): {
+      return {
+        ...state,
+        astronauts: [...state.astronauts, action.payload],
+      }
+    }
+    case getType(removeAstronaut): {
+      const newAstronauts = [...state.astronauts]
+      newAstronauts.pop()
+      return { ...state, astronauts: newAstronauts }
+    }
+    default:
+      return state
+  }
+}
+
+export default reducer
